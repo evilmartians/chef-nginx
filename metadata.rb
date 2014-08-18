1c3740791847b9e4381a35a8178fb2b25104c229
Kirill Kouznetsov <agon.smith@gmail.com>
2014-08-18 23:45:45 +0400

    Version bump: 2.2.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Kirill Kouznetsov'
 maintainer_email  'agon.smith@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures nginx'
-version           '2.2.1'
+version           '2.2.2'
 
 %w( ubuntu debian ).each do |os|
   supports os
