93f4a256ac396c296931c6a155d88b9f798c2b9d
Kirill Kouznetsov <agon.smith@gmail.com>
2015-01-29 15:48:13 +0300

    Version bump: 2.2.4
    Fisrt cookbook dependency: cookbook[apt]

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,13 @@ maintainer        'Kirill Kouznetsov'
 maintainer_email  'agon.smith@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures nginx'
-version           '2.2.3'
+version           '2.2.4'
 
 %w( ubuntu debian ).each do |os|
   supports os
 end
 
+depends 'apt'
+
 recipe 'nginx', "Installs nginx package and sets up configuration similar to Debian's apachesites-enabled/sites-available"
+recipe 'nginx::official-repo', 'Sets up official apt repository on Debian/Ubuntu.'
